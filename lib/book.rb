bee22a8f0bd5b9318c7962529f434ca4cf16428f
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-11 20:33:30 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
-
+class Book
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author_name)
+    @author = author_name
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(book_pages)
+    @page_count = book_pages
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(book_genre)
+    @genre = book_genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,47 @@
-# Make your shoe class here!
+class Shoe
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(shoe_color)
+    @color = shoe_color
+  end
+  
+  def color
+    @color
+  end
+  
+  def size=(shoe_size)
+    @size = shoe_size
+  end
+  
+  def size
+    @size
+  end
+  
+  def material=(shoe_material)
+    @material = shoe_material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(shoe_condition)
+    @condition = shoe_condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
