e6110ad755397b11b84c8ab07185513d7ac06fff
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-11 20:18:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,9 @@
 class Book
-  def initialize(title, author)
+  def initialize(title, author, page_count, genre)
     @title = title
     @author = author
+    @page_count = page_count
+    @genre = genre
   end
   
   def title
@@ -10,6 +12,14 @@ class Book
   
   def author
     @author
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre
+    @genre
   end
   
 end
