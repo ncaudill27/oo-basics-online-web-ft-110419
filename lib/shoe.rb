cf814442ecc3becb20b382bd30d222d02e5581b7
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-11 20:32:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -33,7 +33,7 @@ class Shoe
   end
   
   def condition=(shoe_condition)
-    @shoe_condition
+    @shoe_condition = shoe_condition
   end
   
   def condition
@@ -41,5 +41,7 @@ class Shoe
   end
   
   def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
   end
 end
